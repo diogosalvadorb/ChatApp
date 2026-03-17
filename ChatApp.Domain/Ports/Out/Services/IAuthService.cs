06f8f819207bc6eda4b427d66cb864e559c4abde
diogosalvadorb <diogosalvadorb@gmail.com>
2026-03-17 19:54:43 -0300

    chore: remap arquives and add next front end

--- ChatApp.Domain/Ports/Out/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChatApp.Domain.Ports.Out.Services
-{
-    public interface IAuthService
-    {
-        string HashPassword(string password);
-        bool VerifyPassword(string password, string hash);
-        string GenerateToken(Guid userId, string email);
-    }
-}
